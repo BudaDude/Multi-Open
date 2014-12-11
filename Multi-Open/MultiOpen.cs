1d9c2be36d1ecc1ed27507fbd9eca5ae0ab1b4d1
Buda <thatbuddhadude@gmail.com>
2014-12-10 22:00:36 -0600

    Init Commit

---
+++ Multi-Open/MultiOpen.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Diagnostics;
+
+namespace Multi_Open
+{
+    class MultiOpen
+    {
+        
+        public void OpenFiles()
+        {
+            try
+            {
+
+                string[] lines = File.ReadAllLines("programs.txt");
+                foreach (string path in lines)
+                {
+                    Console.WriteLine(path);
+                    Process.Start(path);
+
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Couldn't open the file. Is the text file in the same dictionary? ");
+                Console.WriteLine(e.Message);
+            }
+
+        }
+    }
+}
---
+++ Multi-Open/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Multi_Open
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MultiOpen mo = new MultiOpen();
+            mo.OpenFiles();
+            
+        }
+    }
+}
